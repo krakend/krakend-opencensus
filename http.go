d9e264372b89b1f3bfd21dd94ddff88990b91576
Philipp Heuer <git@philippheuer.me>
2021-02-10 21:43:33 +0100

    fix: remove dublicate code in http.go

--- http.go
+++ http.go
@@ -7,8 +7,8 @@ import (
 	"github.com/devopsfaith/krakend/config"
 	transport "github.com/devopsfaith/krakend/transport/http/client"
 	"go.opencensus.io/plugin/ochttp"
-	"go.opencensus.io/trace"
 	"go.opencensus.io/tag"
+	"go.opencensus.io/trace"
 )
 
 var defaultClient = &http.Client{Transport: &ochttp.Transport{}}
@@ -25,28 +25,23 @@ func HTTPRequestExecutor(clientFactory transport.HTTPClientFactory, cfg *config.
 		return transport.DefaultHTTPRequestExecutor(clientFactory)
 	}
 	return func(ctx context.Context, req *http.Request) (*http.Response, error) {
-		// tags for stats
-		tags := []tagGenerator{
-			func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.KeyClientHost, req.Host) },
-			func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.Host, req.Host) },
-			func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.KeyClientPath, GetAggregatedPathForBackendMetrics(cfg, req)) },
-			func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.Path, GetAggregatedPathForBackendMetrics(cfg, req)) },
-			func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.KeyClientMethod, req.Method) },
-			func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.Method, req.Method) },
-		}
-		client := clientFactory(ctx)
 		client := clientFactory(ctx)
 		if _, ok := client.Transport.(*Transport); !ok {
 			tags := []tagGenerator{
- 				func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.KeyClientHost, req.Host) },
- 				func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.Host, req.Host) },
- 				func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.KeyClientPath, GetAggregatedPathForBackendMetrics(cfg, req)) },
-	 			func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.Path, GetAggregatedPathForBackendMetrics(cfg, req)) },
- 				func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.KeyClientMethod, req.Method) },
- 				func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.Method, req.Method) },
- 			}
+				func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.KeyClientHost, req.Host) },
+				func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.Host, req.Host) },
+				func(r *http.Request) tag.Mutator {
+					return tag.Upsert(ochttp.KeyClientPath, GetAggregatedPathForBackendMetrics(cfg, req))
+				},
+				func(r *http.Request) tag.Mutator {
+					return tag.Upsert(ochttp.Path, GetAggregatedPathForBackendMetrics(cfg, req))
+				},
+				func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.KeyClientMethod, req.Method) },
+				func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.Method, req.Method) },
+			}
 			client.Transport = &Transport{Base: client.Transport, tags: tags}
 		}
+
 		return client.Do(req.WithContext(trace.NewContext(ctx, fromContext(ctx))))
 	}
 }
