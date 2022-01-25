6e66a728c7f67d17108c3971aa12e92a56add984
kpacha <kpacha666@gmail.com>
2022-01-25 22:43:39 +0100

    minor refactor

--- http.go
+++ http.go
@@ -28,18 +28,29 @@ func HTTPRequestExecutorFromConfig(clientFactory transport.HTTPClientFactory, cf
 	if !IsBackendEnabled() {
 		return transport.DefaultHTTPRequestExecutor(clientFactory)
 	}
+
 	pathExtractor := GetAggregatedPathForBackendMetrics(cfg)
+
 	return func(ctx context.Context, req *http.Request) (*http.Response, error) {
-		client := clientFactory(ctx)
-		if _, ok := client.Transport.(*Transport); !ok {
-			tags := []tagGenerator{
-				func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.KeyClientHost, req.Host) },
-				func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.KeyClientPath, pathExtractor(r)) },
-				func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.KeyClientMethod, req.Method) },
-			}
-			client.Transport = &Transport{Base: client.Transport, tags: tags}
+		httpClient := clientFactory(ctx)
+
+		if _, ok := httpClient.Transport.(*Transport); ok {
+			return httpClient.Do(req.WithContext(trace.NewContext(ctx, fromContext(ctx))))
 		}
 
-		return client.Do(req.WithContext(trace.NewContext(ctx, fromContext(ctx))))
+		c := &http.Client{
+			Transport: &Transport{
+				Base: httpClient.Transport,
+				tags: []tagGenerator{
+					func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.KeyClientHost, req.Host) },
+					func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.KeyClientPath, pathExtractor(r)) },
+					func(r *http.Request) tag.Mutator { return tag.Upsert(ochttp.KeyClientMethod, req.Method) },
+				},
+			},
+			CheckRedirect: httpClient.CheckRedirect,
+			Jar:           httpClient.Jar,
+			Timeout:       httpClient.Timeout,
+		}
+		return c.Do(req.WithContext(trace.NewContext(ctx, fromContext(ctx))))
 	}
 }
